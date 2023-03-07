6c2609fc455d995d1c0a05180faa2b02e13cefe7
radikalkatten <112856496+radikalkatten@users.noreply.github.com>
2023-03-07 16:08:36 +0100

    cookies for name

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,8 @@
 const nameButton = document.getElementById('nameSend')
 const nameInput = document.getElementById('nameInput')
 const productContainer = document.getElementById('productContainer')
-
+const nameForm = document.getElementById('nameForm')
+const customer = document.getElementById('customer')
 const productsURL = 'https://store-7d250-default-rtdb.europe-west1.firebasedatabase.app/products.json'
 
 async function getProducts(){
@@ -21,14 +22,53 @@ async function getProducts(){
     let productTitel = document.createElement('h3')
     let productPrice = document.createElement('p')
     let itemDiv = document.createElement('div')
+    let itemButton = document.createElement('button')
     productImg.src = postData.Image
     productTitel.innerText = `${postData.titel}`
     productPrice.innerText = `Price: ${postData.price}`
+    itemButton.innerText= "add to cart"
     productContainer.appendChild(itemDiv)
     itemDiv.appendChild(productTitel)
     itemDiv.appendChild(productPrice)
     itemDiv.appendChild(productImg)
+    itemDiv.appendChild(itemButton)
   }
 }
 
 getProducts()
+
+function getCookie(uname){
+  let name = uname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie)
+  let ca = decodedCookie.split(';')
+  console.log(ca)
+  for(let i = 0; i <ca.length;i++){
+    let c = ca[i]
+    while (c.charAt(0) == ' '){
+      c = c.substring(1)
+    }
+    if(c.indexOf(name) == 0){
+      return c.substring(name.length, c.length)
+    }
+  }
+  return "";
+}
+function checkCustomerName(){
+  username = getCookie("Username")
+  console.log(username)
+  if(username != ""){
+    let customerName = document.createElement('p')
+    customerName.innerText = `Hello ` + username
+    customer.appendChild(customerName)
+    nameForm.classList.add('inactive')
+  }
+}
+
+
+nameButton.addEventListener('click', (event)=>{
+  // event.preventDefault()
+  console.log('haaa')
+  document.cookie = `Username=${nameInput.value}`
+  
+})
+checkCustomerName()
