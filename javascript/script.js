1fec859dc0420d78705771c6694a92b0e2958fb0
radikalkatten <112856496+radikalkatten@users.noreply.github.com>
2023-03-10 14:34:03 +0100

    typ done

--- javascript/script.js
+++ javascript/script.js
@@ -1,47 +1,157 @@
+import anime from '../node_modules/animejs/lib/anime.es.js';
 const nameButton = document.getElementById('nameSend')
 const nameInput = document.getElementById('nameInput')
 const productContainer = document.getElementById('productContainer')
 const nameForm = document.getElementById('nameForm')
+const bodycount = document.getElementById('bodycount')
 const customer = document.getElementById('customer')
 const productsURL = 'https://store-7d250-default-rtdb.europe-west1.firebasedatabase.app/products.json'
+let bingus = 0
+let diablo3 = 0
+let mclaren = 0
+let foog = 0
+let juan = 0
+let total = bingus + diablo3 + mclaren + foog + juan
+bodycount.innerText = `${total}`
+
+let x = 0
+
+// const myStore = new Store("My store", [
+// ]);
+
+// console.log(myStore);
 
 async function getProducts(){
   const fetchProducts = await fetch(productsURL)
   const data = await fetchProducts.json()
 
   let productID = Object.keys(data)
-  console.log(productID)
+  // console.log(productID)
 
   for(let i = 0; i < productID.length; i++){
     const databaseURLpost = `https://store-7d250-default-rtdb.europe-west1.firebasedatabase.app/products/${productID[i]}.json`
     const fetchPost = await fetch(databaseURLpost)
     const postData = await fetchPost.json()
-
-    console.log(postData.Image)
+    let stockAmount = postData.stock
+    console.log (productID[i])
+    // console.log(postData.Image)
     let productImg = document.createElement('img')
     let productTitel = document.createElement('h3')
     let productPrice = document.createElement('p')
     let itemDiv = document.createElement('div')
     let itemButton = document.createElement('button')
+    let removeButton = document.createElement('button')
+    let stock = document.createElement('p')
     productImg.src = postData.Image
     productTitel.innerText = `${postData.titel}`
     productPrice.innerText = `Price: ${postData.price}`
     itemButton.innerText= "add to cart"
+    removeButton.innerText= "remove from cart"
+    stock.innerText = `Remaining: ${stockAmount}`
     productContainer.appendChild(itemDiv)
     itemDiv.appendChild(productTitel)
     itemDiv.appendChild(productPrice)
     itemDiv.appendChild(productImg)
     itemDiv.appendChild(itemButton)
+    itemDiv.appendChild(stock)
+    itemDiv.appendChild(removeButton)
+    removeButton.classList.add('inactive')
+    itemButton.addEventListener('click', () => {
+      if(stockAmount <= 0){
+        stockAmount = 0
+      }else{
+        checkProduct(productID[i])
+        // pushCart(productID[i], productID[i])
+        removeButton.classList.remove('inactive')
+        console.log(localStorage)
+        stockAmount = stockAmount - 1
+        stock.innerText = `Remaining: ${stockAmount}`
+
+      }
+    })
+    removeButton.addEventListener('click', ()=>{
+      removeProduct(productID[i])
+      console.log(localStorage)
+      stockAmount = stockAmount + 1
+      stock.innerText = `Remaining: ${stockAmount}`
+      if(stockAmount == postData.stock){
+        removeButton.classList.add('inactive')
+      }
+    })
+    // console.log(productTitel.innerText)
   }
 }
-
+// localStorage.clear()
 getProducts()
+function checkProduct(prod){
+  switch (prod){
+    case 'bingus':
+      bingus = bingus + 1;
+      pushCart(prod, bingus)
+      console.log(bingus)
+    break;
+    case 'diablo':
+      diablo3 = diablo3 + 1;
+      pushCart(prod, diablo3)
+      console.log(diablo3)
+    break;
+    case 'juan':
+      juan = juan + 1;
+      pushCart(prod, juan)
+      console.log(juan)
+    break;
+    case 'mclaren':
+      mclaren = mclaren + 1;
+      pushCart(prod, mclaren)
+      console.log(mclaren)
+    break;
+    case 'foog':
+      foog = foog + 1;
+      pushCart(prod, foog)
+      console.log(foog)
+    break;
+    default:
+      console.log('lul')
 
+  }
+}
+function removeProduct(prod){
+  switch (prod){
+    case 'bingus':
+      bingus = bingus - 1;
+      pushCart(prod, bingus)
+      console.log(bingus)
+    break;
+    case 'Diablo 3 eternal':
+      diablo3 = diablo3 - 1;
+      pushCart(prod, diablo3)
+      console.log(diablo3)
+    break;
+    case 'juan':
+      juan = juan - 1;
+      pushCart(prod, juan)
+      console.log(juan)
+    break;
+    case 'Mclaren f1 team':
+      mclaren = mclaren - 1;
+      pushCart(prod, mclaren)
+      console.log(mclaren)
+    break;
+    case 'Lord foog':
+      foog = foog - 1;
+      pushCart(prod, foog)
+      console.log(foog)
+    break;
+    default:
+      console.log('lul')
+
+  }
+}
 function getCookie(uname){
   let name = uname + "=";
   let decodedCookie = decodeURIComponent(document.cookie)
   let ca = decodedCookie.split(';')
-  console.log(ca)
+  // console.log(ca)
   for(let i = 0; i <ca.length;i++){
     let c = ca[i]
     while (c.charAt(0) == ' '){
@@ -54,8 +164,8 @@ function getCookie(uname){
   return "";
 }
 function checkCustomerName(){
-  username = getCookie("Username")
-  console.log(username)
+  const username = getCookie("Username")
+  // console.log(username)
   if(username != ""){
     let customerName = document.createElement('p')
     customerName.innerText = `Hello ` + username
@@ -65,10 +175,85 @@ function checkCustomerName(){
 }
 
 
+async function postName(n){
+  const databaseURLpost = "https://store-7d250-default-rtdb.europe-west1.firebasedatabase.app/customers/.json"
+  const header = {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+  // let customerr = `${n}`
+  // const myCustomer = new Store(`${n}`, [])
+  // const postCustomer = JSON.parse(myCustomer)
+  // // const postCustomer = JSON.stringify(myCustomer)
+
+  const bodyContent = {
+    "name": `${n}`,
+  }
+  const init = {
+    method: "post",
+    body: JSON.stringify(bodyContent),
+    headers: header,
+  }
+  fetch(databaseURLpost, init)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data.name)
+      // data.name
+      const identify = data.name
+      document.cookie = `ID = ${identify}`
+    });
+          
+}
+localStorage.clear()
+
+
+function pushCart(product, number){
+  total = bingus + diablo3 + mclaren + foog + juan
+  bodycount.innerText = `${total}`
+  console.log(total)
+  if(product == localStorage.key(product)){
+    localStorage.setItem(`${product}`, `${number}`)
+  }else{
+    localStorage.setItem(`${product}`, `${number}`)
+  }
+  console.log(localStorage.key(product))
+  // console.log(localStorage)
+  x++
+}
+// const myStorage = localStorage.getItem("myStore")
+// console.log(localStorage)
+// localStorage.clear()
+
 nameButton.addEventListener('click', (event)=>{
-  // event.preventDefault()
+  event.preventDefault()
   console.log('haaa')
+  postName(nameInput.value)
   document.cookie = `Username=${nameInput.value}`
-  
+  console.log(document.cookie)
+  checkCustomerName()
 })
 checkCustomerName()
+
+$("#nameSend").click(function (event) {
+  event.preventDefault();
+  $('.lds-ring').css('visibility', 'initial');
+
+
+  setTimeout(function() {
+    $('.lds-ring').css('visibility', 'hidden');
+    $('#nameSend')
+  }, 1000);
+});
+
+
+const animationInfo = {
+  targets: 'h1',
+  keyframes: [
+      { translateY: '100px', translateX: '100px', backgroundColor: ' hsl(271, 76%, 53%)'},
+      { translateY: '200px', translateX: '50px',  backgroundColor: ' hsl(171, 76%, 53%)' },
+      { translateY: '0px', translateX: '0px',  backgroundColor: ' hsl(50, 76%, 53%)' }
+  ],
+ delay: 1000,
+  duration:1000,
+}
+
+anime(animationInfo);
---
+++ javascript/script2.js
@@ -0,0 +1,106 @@
+const cartDiv = document.getElementById('cartt')
+const clearCartBtn = document.getElementById('button')
+const submitPurchase = document.getElementById('label')
+let total = 0
+let bingus = 250
+let foog = 150
+let diablo3 = 145
+let mclaren = 50
+let juan = 100
+const calculatePrice = []
+function myCart(){
+  let item = localStorage
+  let itemID = Object.values(item)
+  console.log(itemID)
+  let itemName = Object.keys(item)
+  console.log(itemName)
+  console.log(itemID)
+  for(let i = 0; i < localStorage.length;){
+    console.log(localStorage.getItem(itemName[i]))
+    const cart = document.createElement('p')
+    const pricee = document.createElement('p')
+    pricee.innerText = `${itemID[i]}`
+    cart.innerText = `${itemName[i]}`
+    cartDiv.appendChild(pricee)
+    cartDiv.appendChild(cart)
+    calculatePrice.push(itemID[i])
+    // console.log(b)
+    identifyProduct(itemName[i], itemID[i])
+    i++
+  }
+  const sumTotal = document.createElement('p')
+  sumTotal.innerText = `Total: ${total}`
+  cartDiv.appendChild(sumTotal)
+}
+ myCart()
+ function identifyProduct(prod, amount){
+  switch (prod){
+    case 'bingus':
+      total = total + bingus * amount;
+      // console.log(total)
+      
+      // console.log(bingus)
+    break;
+    case 'diablo':
+      total = total + diablo3 * amount
+      
+      // console.log(diablo3)
+    break;
+    case 'juan':
+      total = total + juan * amount
+      
+      // console.log(juan)
+    break;
+    case 'mclaren':
+      total = total + mclaren * amount
+      
+      // console.log(mclaren)
+    break;
+    case 'foog':
+      total = total + foog * amount
+      console.log (total)
+      // console.log(foog)
+    break;
+    default:
+      console.log('lul')
+
+  }
+ }
+
+clearCartBtn.addEventListener('click', ()=>{localStorage.clear()})
+
+async function buyNow(){
+  let item = localStorage
+  let itemID = Object.keys(item)
+  let amount = Object.values(item)
+  console.log(itemID)
+  for(let i=0;i<item.length;i++){
+    let databaseURLget = `https://store-7d250-default-rtdb.europe-west1.firebasedatabase.app/products/${itemID[i]}.json`
+    let data = await fetch(databaseURLget)
+    let database = await data.json()
+    let stock = database.stock
+    stock = stock - amount[i]
+    const header = {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+    const bodyContent = {
+      stock: `${stock}`
+    };
+    const init = {
+      method: "PATCH", 
+      body: JSON.stringify(bodyContent), 
+      headers: header 
+    };
+    fetch(databaseURLget, init)
+      .then(response => response.json())
+      .then(data => console.log(data))
+    
+    console.log(stock)
+  }
+  localStorage.clear()
+}
+
+submitPurchase.addEventListener('click', (event)=>{
+  event.preventDefault()
+  buyNow()
+})
